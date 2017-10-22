25b7832cdf56b501fa35beab508e655b3f45790d
PiGuy2 <rsamm3@gmail.com>
2017-10-21 21:03:08 -0400

    fixes

--- ioServ.py
+++ ioServ.py
@@ -11,7 +11,7 @@ def loadOpts():
     opts = {}
     os.chdir(os.path.dirname(__file__))
     for line in open('opts.txt'):  # load options
-        line = line.strip()
+        line = line.split("#")[0].strip().strip(" ")
         if line and line[0] != "#":
             line = line.strip().split(' : ')
             opts[line[0]] = line[1].split('#')[0].strip(' ')
